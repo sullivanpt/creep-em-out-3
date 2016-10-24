5f24a25af810d63f012918756d858cf8745a0e5c
Patrick Sullivan <sullivanpt@acm.org>
2016-10-23 18:09:48 -0700

    chore: add angular2 material demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
+
+   isDarkTheme: boolean = false;
+
+
+
+  foods: any[] = [
+
+    {name: 'Pizza', rating: 'Excellent'},
+
+    {name: 'Burritos', rating: 'Great'},
+
+    {name: 'French fries', rating: 'Pretty good'},
+
+  ];
+
+
+
+  progress: number = 0;
+
+  constructor() {
+
+    // Update the value for the progress-bar on an interval.
+
+    setInterval(() => {
+
+      this.progress = (this.progress + Math.floor(Math.random() * 4) + 1) % 100;
+
+    }, 200);
+
+  }
 }
